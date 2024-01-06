0b470f6d42760dba4bd5409c5bd969f3d56c8fd9
microinformatica . <micro@Instalador.REDNEURAL.local>
2024-01-07 00:30:55 +0100

    Añadido fuente lusitana

--- app/ui/fonts.tsx
+++ app/ui/fonts.tsx
@@ -1,3 +1,8 @@
 import { New_Rocker } from 'next/font/google'
+import { Lusitana } from 'next/font/google'
 
 export const NewRocker = New_Rocker( { subsets: ['latin'], weight: '400' })
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+  });
