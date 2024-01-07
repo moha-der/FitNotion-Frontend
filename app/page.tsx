debb2621c41bbff49685ff7a82a011c260fba51a
microinformatica . <micro@Instalador.REDNEURAL.local>
2024-01-07 13:47:56 +0100

    Modificado el min-h-screen para no afectar a la imagen

--- app/page.tsx
+++ app/page.tsx
@@ -8,18 +8,18 @@ import { Button } from './ui/button';
 
 export default function Page() {
   return (
-    <main className="grid grid-rows-1 min-h-screen p-6 bg-gray-50 ">
-      <div className='grid grid-cols-10 gap-2 mx-auto'>
+    <main className="grid grid-rows-1 p-6 bg-gray-50">
+      <div className='grid grid-cols-10 gap-4 mx-auto '>
           <div className='col-span-10 md:col-span-5 min-w-full'>
             <div className='flex flex-col items-center py-4 px-5'>
               <h1 className='text-3xl sm:text-4xl lg:text-6xl font-bold'>Equilibra tu Plato <br/> Equilibra tu Vida</h1>
-              <p className='text-base mt-2 text-center'>¿Estás listo para transformar tu relación con la comida? FitNotion es tu aliado.</p>
+              <p className='text-base mt-2 text-center'>¿Estás listo para transformar tu relación con la comida? En FitNotion, cada bocado cuenta hacia un estilo de vida más saludable. </p>
               <button className="flex h-10 flex-col justify-center items-center rounded-lg mt-4 px-4 text-base font-medium text-white transition-colors aria-disabled:cursor-not-allowed aria-disabled:opacity-50 w-full bg-webColor uppercase" > Empieza Gratis
               </button> 
             </div>
           </div>
           <div className='col-span-10 md:col-span-5 p-4'>
-            <Image src={Banner} alt={'Texto'} width={476} height={328} />
+            <Image className='rounded-lg' src={Banner} alt={'Texto'} width={476} height={328} />
           </div>
       </div>
     </main>
